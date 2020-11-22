dfce7651bdb3e78301b86c4a83ded107edc7c0d0
magic-zhu <18817313320@163.com>
2020-11-22 11:25:51 +0800

    feat:更新最大子数组

--- chapter-04/findMaxSubarray.js
+++ chapter-04/findMaxSubarray.js
@@ -3,8 +3,8 @@ function findMaxCrossingSubarray(left, right) {
     const R = right.length
     let sum = left[L - 1]
     let leftSum = left[L - 1], rightSum = right[0]
-    let maxLeft = left[L - 1]
-    let maxRight = right[0]
+    let maxLeft = L-1
+    let maxRight = 0
     for (let i = L - 2; i >= 0; i--) {
         sum = left[i] + sum
         if (leftSum < sum) {
@@ -20,9 +20,8 @@ function findMaxCrossingSubarray(left, right) {
             maxRight = j
         }
     }
-    return [left.slice(maxLeft, L).concat(right.slice(0, maxRight)), leftSum + rightSum]
+    return [left.slice(maxLeft, L).concat(right.slice(0, maxRight + 1)), leftSum + rightSum]
 }
-
 function findMaxSubarray(arr) {
     let newArr = [...arr]
     let len = newArr.length
@@ -41,7 +40,4 @@ function findMaxSubarray(arr) {
         return [crossArr, crossSum]
     }
 }
-let input = [13, -3, -25, 20, -3, -16, -23, 18, 20, -7, -12, -5, -22, 15, -4, 7]
-// let input = [13,-3]
-console.log(findMaxSubarray(input))
-// module.exports = findMaxSubarray
+module.exports = findMaxSubarray
