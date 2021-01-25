27c91f23f261a4d9b31eeb5e1d55d70b83a8e353
magic-zhu <18817313320@163.com>
2021-01-25 15:03:55 +0800

    save

--- chapter-15/OptimalBst.js
+++ chapter-15/OptimalBst.js
@@ -1,6 +1,44 @@
-
-
-
-function OptimalBst(){
-    
+//生成二维数组
+function newTable(m, n) {
+    let arr = [];
+    for (let i = 0; i <= m; i++) {
+        let arrInside = [];
+        for (let j = 0; j <= n; j++) {
+            arrInside.push(0);
+        }
+        arr.push(arrInside);
+    }
+    return arr;
 }
+
+
+function OptimalBst(p, q, n) {
+    let e = newTable(n + 1, n + 1)
+    let w = newTable(n + 1, n + 1)
+    let root = newTable(n, n)
+    for (let i = 1; i <= n + 1; i++) {
+        e[i][i - 1] = q[i - 1]
+        w[i][i - 1] = q[i - 1]
+    }
+    for (let l = 1; l <= n; l++) {
+        for (let i = 1; i <= n - l + 1; i++) {
+            j = i + l - 1
+            e[i][j] = Infinity
+            w[i][j] = w[i][j - 1] + p[j] + q[j]
+            for (let r = i; r <= j; r++) {
+                let t = e[i][r - 1] + e[r + 1][j] + w[i][j]
+                if (t < e[i][j]) {
+                    e[i][j] = t
+                    root[i][j] = r
+                }
+            }
+        }
+    }
+    console.log(e,root)
+    return { e, root }
+}
+
+
+module.exports = OptimalBst
+
+OptimalBst(p, q, n)
