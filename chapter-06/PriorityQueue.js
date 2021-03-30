13edcd2a4291f8a8c41071e58a34d30ac6e04048
magic-zhu <18817313320@163.com>
2021-03-30 17:05:35 +0800

    feat:update

--- chapter-06/PriorityQueue.js
+++ chapter-06/PriorityQueue.js
@@ -92,15 +92,5 @@ class PriorityQueue {
     }
 
 }
-let pq = new PriorityQueue([
-    { priority: 1, todo: '吃饭' },
-    { priority: 2, todo: '洗澡' },
-    { priority: 3, todo: '睡觉' },
-    { priority: 4, todo: '玩手机' },
-    { priority: 5, todo: '打篮球' },
-    { priority: 6, todo: '打乒乓' },
-    { priority: 7, todo: '喝水' }
-])
-pq.increase(6, 8)
-pq.insert({priority: 9, todo: '我是新加的'})
-console.log(pq.queue)
+
+module.exports = PriorityQueue
--- chapter-16/Huffman.js
+++ chapter-16/Huffman.js
@@ -1,16 +1,30 @@
-
 var input = [{ key: 'f', freq: 5 }, { key: 'e', freq: 9 }, { key: 'c', freq: 12 }, { key: 'b', freq: 13 }, { key: 'd', freq: 16 }, { key: 'a', freq: 45 }]
-
+const format = (input) => {
+    return input.map((e) => {
+        e.priority = e.freq
+        return e
+    })
+}
 var MinPriorityQueue = require('../chapter-06/MinPriorityQueue')
+
+function merge() {
+
+}
 
 function Huffman(c) {
     let n = c.length;
-    let Q = c;
-    for (let i = 0; i < n; i++) {
-
+    let Q = new MinPriorityQueue(c);
+    let tree  = [];
+    for (let i = 0; i < n - 1; i++) {
+        let z = { key: '', freq: 0, priority: 0, }
+        let x = Q.extractMin()
+        let y = Q.extractMin()
+        z.priority = x.priority + y.priority
+        z.freq = x.freq + y.freq
+        Q.insert(z)
     }
 }
 
-console.log(Huffman(input))
+console.log(Huffman(format(input)))
 
 module.exports = Huffman
